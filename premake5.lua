2a81628074d849b56584ad3c62bfe65a60243202
CelLumi <73785418+CelLumi@users.noreply.github.com>
2021-04-15 20:25:36 +0200

    fixed premake file

--- premake5.lua
+++ premake5.lua
@@ -2,30 +2,32 @@ project "GLM"
 	kind "StaticLib"
 	language "C"
 
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 	
-	  includedirs { "GLM/" }
+		includedirs { "../GLM/" }
 
-	files
-	{
-		"GLM/glm/**"
-	}
-    
-	filter "system:windows"
-		systemversion "latest"
-		staticruntime "On"
-
-		defines 
-		{ 
-			"_GLM_WIN32",
-			"_CRT_SECURE_NO_WARNINGS"
+		files
+		{
+			"glm/**.hpp",
+			"glm/**.cpp",
+			"glm/**.inl"
 		}
+    
+		filter "system:windows"
+			systemversion "latest"
+			staticruntime "On"
 
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
+			defines 
+			{ 
+				"_GLM_WIN32",
+				"_CRT_SECURE_NO_WARNINGS"
+			}
 
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
+		filter "configurations:Debug"
+			runtime "Debug"
+			symbols "on"
+
+		filter "configurations:Release"
+			runtime "Release"
+			optimize "on"
